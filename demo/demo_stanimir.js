ed337c9f07fc881642f93ee7d8396e1c5c370895
Pavel.Dimitrov <2heads.pd@gmail.com>
2023-01-09 12:23:13 +0200

    add some demo

---
+++ demo/demo_stanimir.js
@@ -0,0 +1,15 @@
+function solve(input) {
+    let index = 0
+                        //0
+    console.log(input[index]);
+    while (true) {
+                            //0
+        console.log(input[index]);
+        index++
+                            //1
+        console.log(input[index]);
+        break;
+    }
+}
+
+solve([1, 2, 3, 4,])
